b0a454ccb21547fa719ce67868990b1fae4e3126
Roger Koradi <code.koradi@gmail.com>
2020-05-30 22:36:45 +0200

    Updates

--- components/PlayerComponent.js
+++ components/PlayerComponent.js
@@ -1,8 +1,9 @@
 class PlayerComponent extends HTMLElement{
-    constructor(){
+    /** @param {String} name the player's name*/
+    constructor(name){
         super()
         this.shadow = this.attachShadow({mode: 'closed'});
-        this.playerName = null
+        this.playerName = name
         this.playerScore = 0
     }
 
@@ -17,17 +18,12 @@ class PlayerComponent extends HTMLElement{
             case 'player':
                 if(oldValue === newValue) return
                 this.playerName = newValue
-                this.updateComponent()
                 break
         }
     }
 
     /** called when element is added to the DOM */
     connectedCallback(){
-        this.updateComponent()
-    }
-
-    updateComponent(){
         const template = `
             <link rel='stylesheet' href='./css/components/player.css'>
             <div class='player'>
@@ -36,14 +32,41 @@ class PlayerComponent extends HTMLElement{
                     <span class='player-score'>${this.playerScore}</span>
                     <button class='btn-delete-player'>X</button>
                 </div>
-                <div class='score-adder'>TODO: allow adding score changes</div>
-                <div class='score-changes'>TODO: display past changes</div>
+                <div class='score-adder'>
+                    <input class='change-is-positive' type='checkbox'>
+                    <input class='change-amount' type='number' min='0'>
+                    <button class='btn-add-change'>OK</button>
+                </div>
+                <div class='score-changes'></div>
             </div>
         `
         this.shadow.innerHTML = template
 
         const deleteButton = this.shadow.querySelector('.btn-delete-player')
         deleteButton.onclick = e => this.parentElement.removeChild(this)
+
+        this.positiveChange = this.shadow.querySelector('.change-is-positive')
+        this.positiveChange.checked = true
+
+        this.changeAmount = this.shadow.querySelector('.change-amount')
+        this.changeList = this.shadow.querySelector('.score-changes')
+
+        const changeOkButton = this.shadow.querySelector('.btn-add-change')
+        changeOkButton.setAttribute('tab-index', -1)
+        changeOkButton.onclick = e => this.addChange()
+    }
+
+    addChange(){
+        const amount = this.changeAmount.value
+        if(!amount) return
+        this.changeAmount.value = null
+        
+        //TODO: replace with actual component
+        const changeText = document.createTextNode((this.positiveChange.checked ? '+' : '-') + amount)
+        const change = document.createElement('div')
+        change.appendChild(changeText)
+
+        this.changeList.prepend(change)
     }
 }
 
